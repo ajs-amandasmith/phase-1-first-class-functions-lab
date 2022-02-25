c5efcda4cd633fb6f92b279d987a7d0c596ff96a
Amanda Smith <ajs.amandasmith@gmail.com>
2022-02-24 18:10:22 -0600

    commit tree 6bd88cece0ccbcdeef027797fc3d6d58147aaa12

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arrayOfDrivers) {
+  return arrayOfDrivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice((drivers.length - 2))
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int) {
+  return function (fare) {
+    return fare * int;
+  }
+}
+
+const fareDoubler = function (num) {
+  return num * 2;
+}
+
+const fareTripler = function(num2) {
+  return num2 * 3;
+}
+
+function selectDifferentDrivers(array, returnFirstTwoDrivers) {
+  return returnFirstTwoDrivers(array);
+}
+// console.log(returnLastTwoDrivers(['pete', 'caroline', 'ben', 'me', 'you']));
